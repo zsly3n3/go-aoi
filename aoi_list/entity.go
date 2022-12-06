25ced4e5c7fb68e603b7ffd0f6d9605348aa7f42
cxp <cxp@pc.com>
2022-12-06 12:58:26 +0800

    add

--- aoi_list/entity.go
+++ aoi_list/entity.go
@@ -1,8 +1,8 @@
 package aoi_list
 
 type Entity struct {
-	UUID   string
-	X      int
-	Y      int
-	Radius uint
+	Radius uint   //广播半径
+	X      int    //x坐标
+	Y      int    //y坐标
+	UUID   string //唯一ID
 }
